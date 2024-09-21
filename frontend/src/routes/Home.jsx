1cf530d735de81eaaf7ff2958d07ed9d48095a10
JasneetSingh-js <139580952+JasneetSingh-js@users.noreply.github.com>
2024-09-21 08:21:00 -0700

    add setprofile

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 import SidebarWithCta from "../components/SidebarWithCta";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import EditProfile from "../components/EditProfile";
+
 
 const Home = () => {
   return (
     <>
       <div className="dark bg-gray-900 text-gray-300 min-h-screen">
         <Header />
-        <EditProfile></EditProfile>
+        
         <div className="flex">
           <SidebarWithCta />
           <main className="mt-16 mb-16 p-4  bg-gray-900 text-gray-300">
